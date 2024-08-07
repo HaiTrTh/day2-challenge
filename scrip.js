b84ff68055d43ae6ed45e321dbbe4c7315b5985b
haitrth <haithanhtran00@gmail.com>
2024-08-07 21:13:00 +0700

    day 2 challenge

---
+++ scrip.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll('.profile__card__img img').addEventListener("mouseover",function(){
+//   document.querySelector('.profile__card__img').style.width = "100%"
+//   document.querySelector('.profile__card__img').style.height = "100%"
+//   console.log(1)
+//   document.querySelector('.profile__card__img img').style.animation = "scale .4s forwards"
+// })
+
+// document.querySelector('.profile__card__img img').addEventListener("mouseout",function(){
+//   document.querySelector('.profile__card__img').style.width = ""
+//   document.querySelector('.profile__card__img').style.height = ""
+//   document.querySelector('.profile__card__img img').style.animation = "shrink .4s forwards"
+// })
+
